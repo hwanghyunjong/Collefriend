34369e05aa967491bade1481c958555a0be2f048
hwanghyunjong <zhwhd3@gmail.com>
2019-11-14 18:55:06 +0900

    이미지 추가, DB 값 받아와서 게시물 구성

--- routes/main.js
+++ routes/main.js
@@ -12,13 +12,20 @@ router.get('/', (req, res) => {
         res.redirect("/login")
     } else {
         var docUserinfo = db.collection('students').doc(`userinfo(${req.session.userid})`);
+        // var docBoardinfo = db.collection(`board(김혁규)`).doc('fSdIFDNrSCLKlpPvWEgz');
         var doc = docUserinfo.get()
             .then(doc => {
                 var name = doc.data().name;
                 var nickname = doc.data().nickname;
+                var boardtime = '2019년 11월 14일 17시 52분';
+                var boardtitle = '혁규';
+                var boardmes = '혀여역규';
                 res.render('../views/index.ejs', {
                     name :  name,
-                    username : nickname
+                    username : nickname,
+                    boardtime : boardtime,
+                    boardtitle : boardtitle,
+                    boardmes : boardmes
                 });
             })
             .catch(err => {
